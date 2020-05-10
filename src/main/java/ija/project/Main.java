55d34b42533f77bcafc1238a0d49910b88f13e8f
SamuelStuchly <sssstuchly@gmail.com>
2020-05-10 21:28:04 +0200

    final ant build and basic program working

--- src/main/java/ija/project/Main.java
+++ src/main/java/ija/project/Main.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -16,10 +17,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main extends Application {
+
+        public static void main(String[] args) {
+                launch(args);
+
+     
+        }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Map.fxml"));
-        BorderPane root = loader.load();
+        if (null == loader.getLocation()){
+                System.out.println("ZAS JE NULL !!!!!!!!");
+
+        }
+
+
+
+       BorderPane root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -75,11 +92,18 @@ public class Main extends Application {
         ObjectMapper mapper = new ObjectMapper(factory);
 
         /** Serializacia **/
-//        mapper.writeValue(new File("data.yml"), data);
+      //  mapper.writeValue(new File("data.yml"), data);
 
+        String filePathString = "data.yml";
         /** Deserializacia **/
+        File f = new File(filePathString);
+        if(f.exists() && !f.isDirectory()) { 
+            System.out.println("IM ALIVE IM A FILE" );
+        }
+        else{System.out.println("NOT A FILE PEPEHANDS");}
+
         Data data1 = mapper.readValue(new File("data.yml"), Data.class);
-//        System.out.println(data1);
+        System.out.println(data1);
 
         elements.addAll(data1.getVehicles());
         elements.addAll(data1.getStops());
@@ -88,5 +112,14 @@ public class Main extends Application {
 
         controller.setElements(elements);
         controller.startTimer(1);
-    }
+  }
+
+
+        @Override
+        public void stop(){
+                System.out.println("Stage is closing");
+                System.exit(0);
+                // Save file
+        }
+
 }
