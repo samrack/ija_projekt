42d0b4bb1806e1b63a3227176db65b2e7c416b5f
SamuelStuchly <sssstuchly@gmail.com>
2020-05-14 05:20:22 +0200

    fixed script so it doesnt download a copy everytime

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-wget https://repo1.maven.org/maven2/org/yaml/snakeyaml/1.23/snakeyaml-1.23.jar
-wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.11.0/jackson-core-2.11.0.jar
-wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.11.0/jackson-databind-2.11.0.jar
-wget https://repo1.maven.org/maven2/com/fasterxml/jackson/dataformat/jackson-dataformat-yaml/2.11.0/jackson-dataformat-yaml-2.11.0.jar
-wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.11.0/jackson-annotations-2.11.0.jar
+
+[ ! -f snakeyaml-1.23.jar ] && wget https://repo1.maven.org/maven2/org/yaml/snakeyaml/1.23/snakeyaml-1.23.jar
+[ ! -f jackson-core-2.11.0.jar ] && wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.11.0/jackson-core-2.11.0.jar
+[ ! -f jackson-databind-2.11.0.jar ] && wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.11.0/jackson-databind-2.11.0.jar
+[ ! -f jackson-dataformat-yaml-2.11.0.jar ] && wget https://repo1.maven.org/maven2/com/fasterxml/jackson/dataformat/jackson-dataformat-yaml/2.11.0/jackson-dataformat-yaml-2.11.0.jar
+[ ! -f jackson-annotations-2.11.0.jar ] && wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.11.0/jackson-annotations-2.11.0.jar
